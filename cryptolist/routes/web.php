4e42b8679ccbf1bb1e4335348e40231b7672ba03
Maxime Theophilos <maxtheo155@icloud.com>
2023-06-30 21:58:52 +0200

    Modification des routes, debug

--- cryptolist/routes/web.php
+++ cryptolist/routes/web.php
@@ -4,5 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CryptocurrencyController;
 
 Route::view('/', 'welcome')->name('home');
-Route::get('/cryptocurrencies', 'CryptocurrencyController@index');
-Route::get('/cryptocurrencies', 'CryptocurrencyController@index')->name('cryptocurrencies.index');
+Route::get('/cryptocurrencies', [CryptocurrencyController::class, 'index'])->name('cryptocurrencies.index');
